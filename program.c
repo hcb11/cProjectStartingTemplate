90632d2bcddc36eefff0c0932ee8be8e03ed1433
hcb11 <64317361+hcb11@users.noreply.github.com>
2021-10-13 15:21:24 +0530

    Add files via upload

---
+++ program.c
@@ -0,0 +1,20 @@
+/* 
+
+    [Name of the app]
+    [Description]
+    
+    Contact Me:
+    
+    Instagram: [Your Instagram Profile Link]
+    Twitter: [Your Twitter Profile Link]
+    Facebook: [Your Facebook Profile Link]
+    
+    Made by [Your Name] on [Date app was made]*/
+
+    #include <stdio.h>
+    int main(void){
+
+        //code
+
+        return 0;
+    }
